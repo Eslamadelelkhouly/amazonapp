1a66fd40a65aeacb70c48f0d8fe850a2ad82e318
Eslamadelelkhouly <rodyerak@gmail.com>
2024-10-13 00:15:10 +0300

    add appbar

---
+++ lib/view/auth_screen/auth_screen.dart
@@ -0,0 +1,46 @@
+import 'package:amazonapp/utils/colors.dart';
+import 'package:flutter/material.dart';
+
+class AuthScreen extends StatefulWidget {
+  const AuthScreen({super.key});
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
+    final textTheme = Theme.of(context).textTheme;
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        backgroundColor: white,
+        title: Image(
+          image: AssetImage('assets/images/amazon_logo.png'),
+          height: height * 0.04,
+        ),
+      ),
+      body: SafeArea(
+        child: Container(
+          height: height,
+          width: width,
+          padding: EdgeInsets.symmetric(
+            horizontal: width * 0.03,
+            vertical: height * 0.02,
+          ),
+          child: Column(
+            children: [
+              Text(
+                'Welcome',
+                style: textTheme.displaySmall,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
