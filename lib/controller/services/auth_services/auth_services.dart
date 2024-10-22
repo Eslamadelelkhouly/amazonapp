0990731eebdec4f7b6cf548f33e09535cb3702ac
Eslamadelelkhouly <rodyerak@gmail.com>
2024-10-22 22:48:43 +0300

    add firebase to project

--- lib/controller/services/auth_services/auth_services.dart
+++ lib/controller/services/auth_services/auth_services.dart
@@ -1,17 +1,31 @@
 import 'dart:developer';
 
+import 'package:amazonapp/view/auth_screen/otp_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:page_transition/page_transition.dart';
 
 class AuthServices {
-  static recieveOTP({required BuildContext context}) async {
+  static recieveOTP({required BuildContext context , required String mobilenumber}) async {
     FirebaseAuth auth = FirebaseAuth.instance;
     try {
       auth.verifyPhoneNumber(
-        verificationCompleted: (PhoneAuthCredential credintial) {},
-        verificationFailed: (FirebaseAuthException exception) {},
-        codeSent: (String verficationID, int? resendToken) {},
-        codeAutoRetrievalTimeout: (String verificationId){},
+        verificationCompleted: (PhoneAuthCredential credintial) {
+          log(credintial.toString());
+        },
+        verificationFailed: (FirebaseAuthException exception) {
+          log(exception.toString());
+        },
+        codeSent: (String verficationID, int? resendToken) {
+          Navigator.push(
+            context,
+            PageTransition(
+              child: OtpScreen(mobilenumber: mobilenumber),
+              type: PageTransitionType.rightToLeft,
+            ),
+          );
+        },
+        codeAutoRetrievalTimeout: (String verificationId) {},
       );
     } catch (e) {
       log(e.toString());
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:amazonapp/firebase_options.dart';
 import 'package:amazonapp/utils/theme.dart';
 import 'package:amazonapp/view/auth_screen/otp_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() {
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const Amazon());
 }
 
@@ -13,9 +19,7 @@ class Amazon extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
-      providers: [
-        
-      ],
+      providers: [],
       child: MaterialApp(
         theme: theme,
         debugShowCheckedModeBanner: false,
